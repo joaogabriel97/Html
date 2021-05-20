7351395bdb2cd42f8bc2d7e26468465a90353508
joaogabriel97 <gabriel_fla04@hotmail.com>
2021-05-20 17:58:09 -0400

    Subindo os códigos referentes aos desafios de Javascript

---
+++ Javacript/desafio 1.js
@@ -0,0 +1,12 @@
+//Como eu faria
+let A = prompt("Digite a quantidade de pimentões Amarelos");
+let B = prompt("Digite a quantidade de pimentões Vermelhos");
+let total = parseInt(A) + parseInt(B);
+console.log("X = " + total);
+
+//O código aceito
+let line = gets().split(" ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = A + B;
+console.log("X = " + total);
