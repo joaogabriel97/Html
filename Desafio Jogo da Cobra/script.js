0ccaf0fbe326991200041f7ef8f4444768b1555e
joaogabriel97 <gabriel_fla04@hotmail.com>
2021-05-24 19:42:21 -0400

    Adicionando áudio de derrota ,setando intervalo para alert de derrota e criando botao de novo jogo

--- Desafio Jogo da Cobra/script.js
+++ Desafio Jogo da Cobra/script.js
@@ -40,18 +40,31 @@ function update(event) {
 }
 
 //Importando o áudio para quando a cobra comer
-function play() {
+function audioComida() {
     var audio = new Audio('comida.mp3');
+    audio.play();
+}
+
+//Importando o áudio em caso de derrota
+function audioDerrota() {
+    var audio = new Audio('derrota.mp3');
     audio.play();
 }
 
 
 function iniciarJogo() {
-
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1 ; i < snake.length ; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            audioDerrota();
+            setTimeout(function(){ alert("Vc perdeu :("); }, 1000);
+        } 
+    }
 
     criarBG();
     criarCobra();
@@ -69,7 +82,7 @@ function iniciarJogo() {
         snake.pop();
     } else{
         //Chamada do áudio para cada comida de fruta
-        play();
+        audioComida();
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
